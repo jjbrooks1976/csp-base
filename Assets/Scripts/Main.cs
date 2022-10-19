0242d2dc2ac96be40ee377d2bb01cb72ad1035a5
jjbrooks1976 <69529883+jjbrooks1976@users.noreply.github.com>
2022-10-19 09:20:53 -0400

    Add partial simulation implementation

--- Assets/Scripts/Main.cs
+++ Assets/Scripts/Main.cs
@@ -1,12 +1,13 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.UI;
 
 public class Main : MonoBehaviour
 {
     public GameObject instance;
+
     public Simulation simulation;
+
+    public GameObject serverDisplayPlayer;
 
     public Toggle errorCorrectionToggle;
     public Toggle correctionSmoothingToggle;
@@ -14,8 +15,8 @@ public class Main : MonoBehaviour
     public Toggle serverPlayerToggle;
     public Text packetLossLabel;
     public Slider packetLossSlider;
-    public Text serverLatencyLabel;
-    public Slider serverLatencySlider;
+    public Text latencyLabel;
+    public Slider latencySlider;
     public Text snapshotRateLabel;
     public Slider snapshotRateSlider;
 
@@ -25,11 +26,14 @@ public class Main : MonoBehaviour
         this.correctionSmoothingToggle.isOn = true;
         this.redundantInputToggle.isOn = true;
         this.serverPlayerToggle.isOn = false;
+        this.packetLossSlider.value = this.simulation.packetLoss;
+        this.latencySlider.value = this.simulation.latency;
+        this.snapshotRateSlider.value = Mathf.Log(this.simulation.snapshotRate, 2.0f);
     }
 
     private void Update()
     {
-        if (Input.GetKeyDown(KeyCode.L))
+        if (Input.GetKeyDown(KeyCode.F12))
         {
             this.instance.SetActive(!this.instance.activeSelf);
         }
@@ -37,33 +41,42 @@ public class Main : MonoBehaviour
 
     public void OnErrorCorrectionToggled(bool value)
     {
+        //this.simulation.errorCorrection = value;
+        this.correctionSmoothingToggle.interactable = value;
+
     }
 
     public void OnCorrectionSmoothingToggled(bool value)
     {
+        this.simulation.correctionSmoothing = value;
     }
 
     public void OnRedundantInputToggled(bool value)
     {
+        this.simulation.redundantInput = value;
     }
 
     public void OnServerPlayerToggled(bool value)
     {
+        this.serverDisplayPlayer.SetActive(value);
     }
 
     public void OnPacketLossChanged(float value)
     {
         this.packetLossLabel.text = string.Format("Packet Loss - {0:F1}%", value * 100.0f);
+        this.simulation.packetLoss = value;
     }
 
-    public void OnServerLatencyChanged(float value)
+    public void OnLatencyChanged(float value)
     {
-        this.serverLatencyLabel.text = string.Format("Server Latency - {0}ms", (int)(value * 1000.0f));
+        this.latencyLabel.text = string.Format("Latency - {0}ms", (int)(value * 1000.0f));
+        this.simulation.latency = value;
     }
 
     public void OnSnapshotRateChanged(float value)
     {
         uint rate = (uint)Mathf.Pow(2, value);
         this.snapshotRateLabel.text = string.Format("Snapshot Rate - {0}hz", 64 / rate);
+        this.simulation.snapshotRate = rate;
     }
 }
--- Assets/Scripts/Simulation.cs
+++ Assets/Scripts/Simulation.cs
@@ -1,9 +1,58 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Simulation : MonoBehaviour
 {
+    private struct Inputs
+    {
+        public bool up;
+        public bool down;
+        public bool left;
+        public bool right;
+        public bool jump;
+    }
+
+    private struct Command
+    {
+        public float deliveryTime;
+        public uint startTickNumber;
+        public List<Inputs> inputs;
+    }
+
+    private struct ClientState
+    {
+        public Vector3 position;
+        public Quaternion rotation;
+    }
+
+    private struct ServerState
+    {
+        public float deliveryTime;
+        public uint tickNumber;
+        public Vector3 position;
+        public Quaternion rotation;
+        public Vector3 velocity;
+        public Vector3 angularVelocity;
+    }
+
+    // common stuff
+    public GameObject clientPlayer;
+    public GameObject smoothedClientPlayer;
+    public GameObject serverPlayer;
+    public GameObject serverDisplayPlayer;
+    public float latency = 0.1f;
+    public float packetLoss = 0.05f;
+
+    // client specific
+    public bool errorCorrection = true;
+    public bool correctionSmoothing = true;
+    public bool redundantInput = true;
+
+    // server specific
+    public uint snapshotRate;
+
     void Start()
     {
     }
